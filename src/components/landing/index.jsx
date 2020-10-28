fcd48371e20c27357f959bea5e02d7e7f2877131
Gangavarapu <mgangavarapu@in.imshealth.com>
2020-10-28 17:35:37 +0530

    adding cover image

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -4,17 +4,27 @@ const LandingPage = () => {
   return (
     <>
       <div className="landing-header">
-        <h2>Hello welcome to Field Expense Tracker..!!</h2>
-        {/* <div className="cover-image">
-          <img src="%PUBLIC_URL%/public/images/green.jpg" alt="cover-image" />
-        </div> */}
-        <strong>
-          Software designed for <span>Mr. Malyadri Reddy </span> garu to help
-          him to manage expenses on his fields
-        </strong>
-      </div>
-      <div style={{ textAlign: "end", color: "darkslateblue" }}>
-        <strong>--Designed and Developed by Manoj Reddy Gangavarapu</strong>
+        <h1 style={{ color: "white", fontFamily: "fantasy" }}>
+          Hello welcome to Field Expense Tracker{" "}
+          <span role="img" aria-labelledby="cat">
+            ✊
+          </span>
+        </h1>
+        <h3>
+          <strong>
+            Software designed for <span>Mr. Malyadri Reddy </span> garu to
+            manage expenses on his fields.
+          </strong>
+        </h3>
+
+        <div style={{ marginTop: "20%", color: "white" }}>
+          <strong>
+            Designed and Developed by Manoj Reddy Gangavarapu with{" "}
+            <span role="img" aria-labelledby="cat">
+              😻
+            </span>
+          </strong>
+        </div>
       </div>
     </>
   );
